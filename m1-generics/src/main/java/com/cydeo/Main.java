a3d6c26cbe91ae397fdbb56fc2bdacdaefe88fdc
SelmanKarakas101 <selmankarakas1@gmail.com>
2023-07-26 16:55:22 +0300

    Generics

--- m1-generics/src/main/java/com/cydeo/Main.java
+++ m1-generics/src/main/java/com/cydeo/Main.java
@@ -14,6 +14,19 @@ public class Main {
 
         printDoubled(items);
 
+        System.out.println("----------------------------");
+
+        FootballPlayer joe = new FootballPlayer("joe");
+        BaseballPlayer pat = new BaseballPlayer("Pat");
+        SoccerPlayer beckham = new SoccerPlayer("Beckham");
+
+        Team<SoccerPlayer> liverpool = new Team("Liverpool");
+        liverpool.addPlayer(beckham);
+
+        //Team<String> brokenTeam = new Team<>("what kind of team is this?");
+
+        //Team<Integer> t2 = new Team<>("Very weird");
+
     }
 
     private static void printDoubled(ArrayList<Integer> items) {
