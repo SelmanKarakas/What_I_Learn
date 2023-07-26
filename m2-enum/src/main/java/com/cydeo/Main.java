27c783d8202d9c677b0f7c30125b70def3d39d01
SelmanKarakas101 <selmankarakas1@gmail.com>
2023-07-26 18:25:02 +0300

    Enum

--- m2-enum/src/main/java/com/cydeo/Main.java
+++ m2-enum/src/main/java/com/cydeo/Main.java
@@ -7,6 +7,55 @@ public class Main {
         System.out.println("How to access a constant");
 
         Currency c = Currency.DIME;
+        System.out.println(c);
+        System.out.println(Currency.PENNY);
+
+        System.out.println("How to get all constant");
+        Currency[] currencies = Currency.values();
+
+        for (Currency currency : currencies){
+            System.out.println(currency);
+        }
+
+        System.out.println("How to use switch-case with enums");
+
+        switch (Currency.DIME){
+            case PENNY:
+                System.out.println("Is is 1 cent");
+                break;
+            case NICKLE:
+                System.out.println("It is 5 cent");
+                break;
+            case DIME:
+                System.out.println("It is 10 cent");
+                break;
+            case QUARTER:
+                System.out.println("Is is 25 cent");
+                break;
+        }
+
+    }
+
+    private static void calculate(double x, double y, Operation operation){
+
+        switch (operation){
+            case PLUS:
+                System.out.println(x+y);
+                break;
+            case MINUS:
+                System.out.println(x-y);
+                break;
+            case MULTIPLY:
+                System.out.println(x*y);
+                break;
+            case DIVIDE:
+                System.out.println(x/y);
+                break;
+        }
+
+        System.out.println("Operation Task");
+        calculate(3,5,Operation.MULTIPLY);
+
     }
 
 }
