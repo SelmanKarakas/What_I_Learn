cb5f49f7e2b571e57c49d6597065bb4c8f88df1d
SelmanKarakas101 <selmankarakas1@gmail.com>
2023-08-06 20:00:16 +0300

    comparator interface

--- m5-comparator_interface/src/main/java/com/cydeo/Main.java
+++ m5-comparator_interface/src/main/java/com/cydeo/Main.java
@@ -14,5 +14,11 @@ public class Main {
         //Ascending
         Collections.sort(list);
         System.out.println(list);
+
+        //Descending
+        Collections.sort(list,new MyComparator());
+        System.out.println(list);
+
+        Collections.sort(list,((o1,o2) -> (o1>o2) ? -1 : (o1<o2) ? 1 : 0));
     }
 }
