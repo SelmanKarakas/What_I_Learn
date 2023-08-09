fcfc20ca246a6e662f30df8b740af78d8216d74c
SelmanKarakas101 <selmankarakas1@gmail.com>
2023-08-09 19:02:16 +0300

    single responsibility, principle good example

---
+++ solid_principles/src/main/java/com/cydeo/solid/singleResonsibility/example/good/Manager.java
@@ -0,0 +1,15 @@
+package com.cydeo.solid.singleResonsibility.example.good;
+
+public class Manager {
+
+    public static boolean validate(){
+        //Chech validation
+        return true;
+    }
+
+    public static boolean checkSecurity(){
+        //Send to security related part
+        return true;
+    }
+
+}
