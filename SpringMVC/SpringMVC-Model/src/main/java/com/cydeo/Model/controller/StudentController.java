5e01e1a3679abf4c0080e1ccf6f6929861d8ae5b
SelmanKarakas101 <selmankarakas1@gmail.com>
2023-08-22 02:16:14 +0300

    Spring Training - Spring MVC

--- SpringMVC/SpringMVC-Model/src/main/java/com/cydeo/Model/controller/StudentController.java
+++ SpringMVC/SpringMVC-Model/src/main/java/com/cydeo/Model/controller/StudentController.java
@@ -1,13 +1,19 @@
 package com.cydeo.Model.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class StudentController {
 
     @RequestMapping("/welcome")
-    public String homePage(){
-        String name = "Ozzy";
+    public String homePage(Model model){
 
+        model.addAttribute("name","Cydeo");
+        model.addAttribute("course","MVC");
+
+        return "student/welcome";
     }
 
 }
