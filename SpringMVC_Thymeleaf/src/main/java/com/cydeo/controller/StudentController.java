061944e3344c26444683e1ca661c86efc0bb26cd
SelmanKarakas101 <selmankarakas1@gmail.com>
2023-08-29 22:00:58 +0300

    SpringMVC Training - Thymeleaf

--- SpringMVC_Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ SpringMVC_Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -20,9 +20,10 @@ public class StudentController {
     }
 
     @RequestMapping("/welcome")
-    public String welcome(@RequestParam int id){
+    public String welcome(@RequestParam String name){
 
-        System.out.println(id);
+        System.out.println(name);
+
         return "student/welcome";
     }
 
