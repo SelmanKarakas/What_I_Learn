fae0664f9dc3bb2ceac65df792cf34df18897cdd
SelmanKarakas101 <selmankarakas1@gmail.com>
2023-08-30 23:23:58 +0300

    SpringMVC Training - Thymeleaf

--- SpringMVC_Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ SpringMVC_Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
 @RequestMapping("/student")
 public class StudentController {
