114b9b8c3b05c8c382fcb5064ee08175183a59ab
SelmanKarakas101 <selmankarakas1@gmail.com>
2023-08-31 01:43:01 +0300

    SpringMVC Training - Thymeleaf

--- SpringMVC_Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ SpringMVC_Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -1,21 +1,16 @@
 package com.cydeo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/mentor")
 public class MentorController {
 
-    @RequestMapping("/register")
+    @GetMapping("/register")
     public String register(){
 
-        return "student/register";
-    }
-
-    @RequestMapping("/drop")
-    public String drop(){
-
-        return "student/register";
+        return "student/mentor-register";
     }
 }
--- SpringMVC_Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ SpringMVC_Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -3,13 +3,14 @@ package com.cydeo.controller;
 import com.cydeo.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
 
-    @RequestMapping("/register")
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    @GetMapping("/register")
     public String register(Model model){
 
         model.addAttribute("students", DataGenerator.createStudent());
@@ -17,8 +18,9 @@ public class StudentController {
         return "student/register";
     }
 
-    @RequestMapping("/welcome")
-    public String welcome(){
+    @RequestMapping(value = "/welcome", method = RequestMethod.POST)
+    @PostMapping("/welcome")
+    public String welcome(@RequestParam String name){
 
         return "student/welcome";
     }
