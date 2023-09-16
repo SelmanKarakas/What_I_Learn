5eb35ca4b6d57ce958917b0f34c2fb4ef8176646
SelmanKarakas101 <selmankarakas1@gmail.com>
2023-09-16 19:38:45 +0300

    Spring Data

--- Spring/SpringData/SpringData_TableCreation/src/main/resources/data.sql
+++ Spring/SpringData/SpringData_TableCreation/src/main/resources/data.sql
@@ -1,2 +1,7 @@
 INSERT INTO EMPLOYEES(id,name)
 VALUES (1,'Mike');
+
+INSERT INTO students(,first_name,last_name,email)
+VALUES (1,'Selman','Karakas','selman@cydeo.com');
+INSERT INTO students(,first_name,last_name,email)
+VALUES ('Sıla','Hanim','zılaben@cydeo.com');
