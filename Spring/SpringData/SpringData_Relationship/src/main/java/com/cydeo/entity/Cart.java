965bffa0ef523a141777ce35c0e92848ace1c941
SelmanKarakas101 <selmankarakas1@gmail.com>
2023-09-17 19:09:33 +0300

    Spring Data - Relationship

---
+++ Spring/SpringData/SpringData_Relationship/src/main/java/com/cydeo/entity/Cart.java
@@ -0,0 +1,20 @@
+package com.cydeo.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String code;
+
+}
