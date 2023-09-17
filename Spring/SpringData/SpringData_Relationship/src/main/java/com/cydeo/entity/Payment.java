d4abab650c7e81eb7ba4982d8dc5fc8431956ae4
SelmanKarakas101 <selmankarakas1@gmail.com>
2023-09-17 18:33:37 +0300

    Spring Data - Relationship

--- Spring/SpringData/SpringData_Relationship/src/main/java/com/cydeo/entity/Payment.java
+++ Spring/SpringData/SpringData_Relationship/src/main/java/com/cydeo/entity/Payment.java
@@ -26,7 +26,7 @@ public class Payment {
     @Enumerated(EnumType.STRING)
     private Status paymentStatus;
 
-    @OneToOne
+    @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "payment_detail_id")
     private PaymentDetail paymentDetail;
 
