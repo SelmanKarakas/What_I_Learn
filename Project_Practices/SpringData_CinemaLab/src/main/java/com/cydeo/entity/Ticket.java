aa7128ee29e4f6acc419707a1ec11c62dc0e289f
SelmanKarakas101 <selmankarakas1@gmail.com>
2023-09-20 22:24:01 +0300

    Spring Data - Cinema Lab Project

--- Project_Practices/SpringData_CinemaLab/src/main/java/com/cydeo/entity/Ticket.java
+++ Project_Practices/SpringData_CinemaLab/src/main/java/com/cydeo/entity/Ticket.java
@@ -1,7 +1,6 @@
 package com.cydeo.entity;
 
-import com.cydeo.entity.BaseEntity;
-import com.cydeo.entity.Movie;
+import com.fasterxml.jackson.databind.ser.Serializers;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -14,17 +13,16 @@ import java.time.LocalDateTime;
 @Entity
 @Data
 @NoArgsConstructor
-public class MovieCinema extends BaseEntity {
-    
+public class Ticket extends BaseEntity {
+
+    private Integer seatNumber;
+    private Integer rowNumber;
+
     @Column(columnDefinition = "TIMESTAMP")
     private LocalDateTime dateTime;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    private Movie movie;
+    private MovieCinema movieCinema;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    private Cinema cinema;
-
-
-    
+    private User userAccount;
 }
