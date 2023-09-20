f418bb8f98795df90c8187f046ffa9f9d92ea55c
SelmanKarakas101 <selmankarakas1@gmail.com>
2023-09-20 21:39:49 +0300

    Spring Data - Cinema Lab Project

--- Project_Practices/SpringData_CinemaLab/src/main/java/com/cydeo/entity/Movie.java
+++ Project_Practices/SpringData_CinemaLab/src/main/java/com/cydeo/entity/Movie.java
@@ -1,32 +1,35 @@
 package com.cydeo.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.xml.stream.Location;
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Entity
+@Data
 @NoArgsConstructor
 @Getter
 @Setter
-public class Movie {
+public class Movie extends BaseEntity {
 
     private String name;
+
     @Column(columnDefinition = "DATE")
     private LocalDate releaseDate;
     private Integer duration;
 
-    @Override
-    public String toString() {
-        return "Cinema{" +
-                "name='" + name + '\'' +
-                ", sponsoredName='" + //sponsoredName + '\'' +
-                '}';
-    }
-}
+    @Column(columnDefinition = "text")
+    private String summary;
+
+    @Enumerated(EnumType.STRING)
+    private MovieType type;
+
+    @Enumerated(EnumType.STRING)
+    private MovieState state;
+
+    private BigDecimal price;
