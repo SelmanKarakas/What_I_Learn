ffa127100381b8f37b4edcb1c57f59695e78aa74
SelmanKarakas101 <selmankarakas1@gmail.com>
2023-09-21 17:47:44 +0300

    Spring Data - Queries

---
+++ Spring/SpringData/SpringData_Queries/src/main/java/com/cydeo/entity/Employee.java
@@ -0,0 +1,11 @@
+package com.cydeo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "employees")
+@NoArgsConstructor
+public class Employee {
+}
