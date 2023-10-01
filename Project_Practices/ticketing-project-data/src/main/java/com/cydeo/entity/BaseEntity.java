5fa42bc63350193c11bfe05fcc8c0491389ca0fc
SelmanKarakas101 <selmankarakas1@gmail.com>
2023-10-01 20:45:40 +0300

    Spring Data - Ticketing App

--- Project_Practices/ticketing-project-data/src/main/java/com/cydeo/entity/BaseEntity.java
+++ Project_Practices/ticketing-project-data/src/main/java/com/cydeo/entity/BaseEntity.java
@@ -1,16 +1,23 @@
 package com.cydeo.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@MappedSuperclass
+@Getter
+@Setter
 public class BaseEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private LocalDateTime insertDateTime;
     private Long insertUserId;
