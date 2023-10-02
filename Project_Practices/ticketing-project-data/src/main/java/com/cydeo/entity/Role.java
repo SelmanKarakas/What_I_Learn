00f22fafd568fed0aa3efb08805aa4a246afb2b0
SelmanKarakas101 <selmankarakas1@gmail.com>
2023-10-03 01:37:46 +0300

    Spring Data - Ticketing App

--- Project_Practices/ticketing-project-data/src/main/java/com/cydeo/entity/Role.java
+++ Project_Practices/ticketing-project-data/src/main/java/com/cydeo/entity/Role.java
@@ -11,7 +11,7 @@ import javax.persistence.Table;
 @AllArgsConstructor
 @Entity
 @Table(name = "roles")
-public class Role extends BaseEntity{
+public class Role extends BaseEntity {
 
     private String description;
 
