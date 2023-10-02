163c9cc808ab4341a005ddd2cdb4b4b370942499
SelmanKarakas101 <selmankarakas1@gmail.com>
2023-10-03 01:38:07 +0300

    Spring Data - Ticketing App

--- Project_Practices/ticketing-project-data/src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
+++ Project_Practices/ticketing-project-data/src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
@@ -5,12 +5,12 @@ import com.cydeo.entity.Role;
 import com.cydeo.mapper.RoleMapper;
 import com.cydeo.repository.RoleRepository;
 import com.cydeo.service.RoleService;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Repository
+@Service
 public class RoleServiceImpl implements RoleService {
 
     private final RoleRepository roleRepository;
@@ -24,11 +24,17 @@ public class RoleServiceImpl implements RoleService {
     @Override
     public List<RoleDTO> listAllRoles() {
 
-        return roleRepository.findAll().stream().map(roleMapper::convertToDto).collect(Collectors.toList());
+        List<Role> roleList = roleRepository.findAll();
+
+        return roleList.stream().map(roleMapper::convertToDto).collect(Collectors.toList());
     }
 
     @Override
     public RoleDTO findById(Long id) {
-        return null;
+
+
+
+
+        return roleMapper.convertToDto(roleRepository.findById(id).get());
     }
 }
