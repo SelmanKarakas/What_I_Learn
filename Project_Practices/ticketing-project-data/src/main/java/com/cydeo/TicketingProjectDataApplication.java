bb4c5b4634f944d84f090b1d3fe4651d070f58ac
SelmanKarakas101 <selmankarakas1@gmail.com>
2023-10-03 01:38:24 +0300

    Spring Data - Ticketing App

--- Project_Practices/ticketing-project-data/src/main/java/com/cydeo/TicketingProjectDataApplication.java
+++ Project_Practices/ticketing-project-data/src/main/java/com/cydeo/TicketingProjectDataApplication.java
@@ -4,12 +4,12 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.ui.Model;
 
-@SpringBootApplication
+@SpringBootApplication  //this includes @Configuration
 public class TicketingProjectDataApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(TicketingProjectDataApplication.class, args);
     }
 
@@ -17,5 +17,9 @@ public class TicketingProjectDataApplication {
     public ModelMapper mapper(){
         return new ModelMapper();
     }
+
+
+
+
 
 }
