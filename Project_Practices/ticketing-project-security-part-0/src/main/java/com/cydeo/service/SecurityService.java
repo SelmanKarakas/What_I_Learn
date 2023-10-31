474c40ca18b7985fd03462b833c0bbf0b377fb04
SelmanKarakas101 <selmankarakas1@gmail.com>
2023-10-31 18:58:06 +0300

    Spring Security - Ticketing App

---
+++ Project_Practices/ticketing-project-security-part-0/src/main/java/com/cydeo/service/SecurityService.java
@@ -0,0 +1,10 @@
+package com.cydeo.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public interface SecurityService extends UserDetailsService {
+
+
+}
