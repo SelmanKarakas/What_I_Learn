013da7dd32e36ecd46d13625d2dab784834b951c
SelmanKarakas101 <selmankarakas1@gmail.com>
2023-11-15 02:47:57 +0300

    Spring REST

--- Spring/SpringRest/SpringRest/src/main/java/com/cydeo/dto/ResponseWrapper.java
+++ Spring/SpringRest/SpringRest/src/main/java/com/cydeo/dto/ResponseWrapper.java
@@ -10,22 +10,22 @@ import org.springframework.http.HttpStatus;
 @NoArgsConstructor
 public class ResponseWrapper {
 
-    private boolean success;
-    private String message;
-    private Integer code;
-    private Object data;
+   private boolean success;
+   private String message;
+   private Integer code;
+   private Object data;
 
-    public ResponseWrapper(String message,Object data){
-        this.message = message;
-        this.data=data;
-        this.code= HttpStatus.OK.value();
-        this.success=true;
-    }
+   public ResponseWrapper(String message, Object data){
+       this.message = message;
+       this.data = data;
+       this.code = HttpStatus.OK.value();
+       this.success=true;
+   }
 
-    public ResponseWrapper(String message){
-        this.message=message;
-        this.code=HttpStatus.OK.value();
-        this.success=true;
-    }
+   public ResponseWrapper(String message){
+       this.message=message;
+       this.code=HttpStatus.OK.value();
+       this.success=true;
+   }
 
 }
