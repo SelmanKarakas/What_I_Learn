dcfcdd6bbc4463836347fda0e158146260bdf6b8
SelmanKarakas101 <selmankarakas1@gmail.com>
2023-12-05 23:52:15 +0300

    Spring REST

---
+++ Spring/SpringRest/SpringRest_ConsumeApis/src/main/java/com/cydeo/SpringRestConsumeApisApplication.java
@@ -0,0 +1,22 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableFeignClients
+public class SpringRestConsumeApisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestConsumeApisApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+}
